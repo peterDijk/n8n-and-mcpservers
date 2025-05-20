cb144f79ee5edc3bad4ddf7d838d6d32fea75555
Peter van Dijk <peter.van.dijk@bynder.com>
2025-05-20 11:08:11 +0200

    start of stdio setup

--- test-mcp-server/src/index.ts
+++ test-mcp-server/src/index.ts
@@ -3,7 +3,7 @@ import { createSSEServer } from "./sse-server.ts";
 import { createStreamHttpServer } from "./stream-http-server.ts";
 
 const mcpServer = create();
-// const sseServer = createSSEServer(mcpServer);
+const sseServer = createSSEServer(mcpServer);
 const streamableHttpServer = createStreamHttpServer(mcpServer);
 
-streamableHttpServer.listen(process.env.PORT || 3002);
+sseServer.listen(process.env.PORT || 3002);
